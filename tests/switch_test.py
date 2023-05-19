6cc9288a7beed5cc1bc88e2e76a24b7535e18cc8
g8bsy <58439789+g8bsy@users.noreply.github.com>
2023-05-19 15:29:20 +0100

    d

--- tests/switch_test.py
+++ tests/switch_test.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(5, GPIO.IN)
-GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     print(str(GPIO.input(5)) + "-" + str(GPIO.input(6)))
